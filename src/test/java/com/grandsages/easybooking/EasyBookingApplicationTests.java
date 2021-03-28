dccc858d243290bb9d6335adcf3d8ad1bfbff1d8
H.J. Liau <ifutheyou@hotmail.com>
2021-03-28 21:15:23 +0800

    Initial commit

---
+++ src/test/java/com/grandsages/easybooking/EasyBookingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.grandsages.easybooking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EasyBookingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
