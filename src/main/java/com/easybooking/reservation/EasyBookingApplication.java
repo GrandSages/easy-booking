82430cf56362984b36ede034c123795fff8f4c37
H.J. Liau <ifutheyou@hotmail.com>
2021-06-06 23:22:13 +0800

    rename package into micro service style

--- src/main/java/com/easybooking/reservation/EasyBookingApplication.java
+++ src/main/java/com/easybooking/reservation/EasyBookingApplication.java
@@ -1,4 +1,4 @@
-package com.grandsages.easybooking;
+package com.easybooking.reservation;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/com/easybooking/reservation/EasyBookingApplicationTests.java
+++ src/test/java/com/easybooking/reservation/EasyBookingApplicationTests.java
@@ -1,4 +1,4 @@
-package com.grandsages.easybooking;
+package com.easybooking.reservation;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
